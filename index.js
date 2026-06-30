badef5ea3b50be0835b7e072f1d3687277e1de8d
educ-jkescritor <judekeithescritor@gmail.com>
2026-06-30 21:45:18 +0800

    feat: build main UI, cat selection layout, and window controls

---
+++ index.js
@@ -0,0 +1,14 @@
+let orangeButton = document.getElementById("orange_cat");
+orangeButton.onclick = function orangeScreen() {
+    window.location.assign("timer/orange_timer/orange_timer.html");
+}
+
+let tuxedoButton = document.getElementById("tuxedo_cat");
+tuxedoButton.onclick = function tuxedoScreen() {
+    window.location.assign("timer/tuxedo_timer/tuxedo_timer.html");
+}
+
+let blackButton = document.getElementById("black_cat");
+blackButton.onclick = function blackScreen() {
+    window.location.assign("timer/black_timer/black_timer.html");
+}
---
+++ titlebar.js
@@ -0,0 +1,19 @@
+let settingsButton = document.getElementById("settings-btn");
+settingsButton.onclick = function settingsWindow() {
+    window.mainAPI.settings();
+}
+
+let minimizeButton = document.getElementById("minimize-btn");
+minimizeButton.onclick = function minimizeWindow() {
+    window.mainAPI.minimize();
+}
+
+let maximizeButton = document.getElementById("maximize-btn");
+maximizeButton.onclick = function maximizeWindow() {
+    window.mainAPI.maximize();
+}
+
+let closeButton = document.getElementById("close-btn");
+closeButton.onclick = function closeWindow() {
+    window.mainAPI.close();
+}
